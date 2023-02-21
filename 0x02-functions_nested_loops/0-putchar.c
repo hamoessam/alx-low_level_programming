aa0520c6779bb7eab2afdf23237dfb916b943328
hamoessam <hamoessam523@gmail.com>
2023-02-21 12:57:12 -0800

    0.task

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,16 +6,14 @@
  */
 int main(void)
 {
-	char *a = "_putchar";
-
-	while (*a)
-	{
-
-		_putchar(*a);
-		a++;
-	}
-
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
-
 	return (0);
 }
